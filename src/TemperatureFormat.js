4536e7adedaf3eb44212a7ff615ec74922c1916d
Annabel1991 <125332330+Annabel1991@users.noreply.github.com>
2023-06-03 00:31:12 -0700

    Added unites convertation

--- src/TemperatureFormat.js
+++ src/TemperatureFormat.js
@@ -1,10 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TemperatureFormat(props) {
-  return (
-    <div className="TemperatureFormat">
-      <span className="temperature">{Math.round(props.fahrenheit)}</span>
-      <span className="unit">°F</span>
-    </div>
-  );
+  const [unit, setUnit] = useState("fahrenheit");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="TemperatureFormat">
+        <span className="temperature">{Math.round(props.celsius)}</span>
+        <span className="unit">
+          ° C |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            °F
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="TemperatureFormat">
+        <span className="temperature">{Math.round(fahrenheit())}</span>
+        <span className="unit">
+          <a href="/" onClick={showCelsius}>
+            °C
+          </a>{" "}
+          | °F
+        </span>
+      </div>
+    );
+  }
 }
