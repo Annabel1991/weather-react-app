aaed7ca0aee47eff9c7292adc449c7342a7fb307
Annabel1991 <125332330+Annabel1991@users.noreply.github.com>
2023-06-03 00:38:43 -0700

    Update TemperatureFormat.js

--- src/TemperatureFormat.js
+++ src/TemperatureFormat.js
@@ -18,7 +18,7 @@ export default function TemperatureFormat(props)  {
 
   if (unit === "celsius") {
     return (
-      <div className="WeatherTemperature">
+      <div className="TemperatureFormat">
         <span className="temperature">{Math.round(props.celsius)}</span>
         <span className="unit">
           ° C |{" "}
@@ -30,7 +30,7 @@ export default function TemperatureFormat(props)  {
     );
   } else {
     return (
-      <div className="WeatherTemperature">
+      <div className="TemperatureFormat">
         <span className="temperature">{Math.round(fahrenheit())}</span>
         <span className="unit">
           <a href="/" onClick={showCelsius}>
