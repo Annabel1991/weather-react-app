e420e0c9d4d2e1bbff1c832dfd769035b17c566b
Annabel1991 <125332330+Annabel1991@users.noreply.github.com>
2023-06-03 00:08:39 -0700

    Update WeatherCurrent.js

--- src/WeatherCurrent.js
+++ src/WeatherCurrent.js
@@ -6,9 +6,9 @@ import TemperatureFormat from "./TemperatureFormat";
 export default function WeatherCurrent(props) {
   return (
     <div className="WeatherCurrent">
-      <div className="row mt-3">
+      <div className="row">
       <div className="col-5">
-          <div className="d-flex">
+     
       <h1>{props.data.city}</h1>
       <ul>
         <li>
@@ -17,18 +17,10 @@ export default function WeatherCurrent(props) {
         <li className="text-capitalize">{props.data.description}</li>
       </ul>
       </div>
-      </div>
       <div className="col-3">
-          <div className="d-flex">
-            <div>
               <Icon code={props.data.icon} size={55} />
-            </div>
-
-            <div>
               <TemperatureFormat fahrenheit={props.data.temperature} />
             </div>
-          </div>
-        </div>
         <div className="col-4">
           <ul>
             <li>Humidity: {props.data.humidity}%</li>
